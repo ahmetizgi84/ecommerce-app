3e689360d0419e5089e0d228fb63a8b7b4dc1755
Ahmet IZGI <dev.ahmetizgi@gmail.com>
2023-04-02 16:07:07 +0300

    chore(*): test change

--- src/components/Jumbotron.vue
+++ src/components/Jumbotron.vue
@@ -10,6 +10,7 @@
         <h1 class="mb-4 text-4xl font-extrabold tracking-tight leading-none text-black md:text-5xl lg:text-8xl px-8">Clothes</h1>
         
         <p class="mb-8 text-lg font-normal text-gray-700 lg:text-xl px-8">Live for Influential and Innovative fashion!</p>
+        <p class="mb-8 text-lg font-normal text-gray-700 lg:text-xl px-8">TEST CHANGE</p>
         
         <!-- <div class="flex flex-col space-y-4 sm:flex-row sm:justify-center sm:space-y-0 sm:space-x-4">
             <a href="#" class="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900">
