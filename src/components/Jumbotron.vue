a85d9fb1f89fe8d9372a4ef93c84b4754e23a5b7
Ahmet İZGİ <dev.ahmetizgi@gmail.com>
2023-04-08 18:48:50 +0300

    chore(*): tailwind.config.js

--- src/components/Jumbotron.vue
+++ src/components/Jumbotron.vue
@@ -10,17 +10,7 @@
         <h1 class="mb-4 text-4xl font-extrabold tracking-tight leading-none text-black md:text-5xl lg:text-8xl px-8">Clothes</h1>
         
         <p class="mb-8 text-lg font-normal text-gray-700 lg:text-xl px-8">Live for Influential and Innovative fashion!</p>
-        <p class="mb-8 text-lg font-normal text-gray-700 lg:text-xl px-8">TEST CHANGE</p>
         
-        <!-- <div class="flex flex-col space-y-4 sm:flex-row sm:justify-center sm:space-y-0 sm:space-x-4">
-            <a href="#" class="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900">
-                Get started
-                <svg aria-hidden="true" class="ml-2 -mr-1 w-4 h-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
-            </a>
-            <a href="#" class="inline-flex justify-center hover:text-gray-900 items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg border border-white hover:bg-gray-100 focus:ring-4 focus:ring-gray-400">
-                Learn more
-            </a>  
-        </div> -->
     </div>
 </section>
 </template>
